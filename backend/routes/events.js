179e0865edd6e20cceb3f094765450e37c34cd74
Simon <57576661+simon-stentoft@users.noreply.github.com>
2023-09-01 19:33:29 +0200

    starting on backend

---
+++ backend/routes/events.js
@@ -0,0 +1,32 @@
+const express = require("express")
+
+const router = express.Router()
+
+//GET all events
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all events'})
+})
+
+//GET single event
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET a single event'})
+})
+
+//POST a new event
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST an event'})
+})
+
+//DELETE an event
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'DELETE an event'})
+})
+
+//UPDATE/PATCH an event
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'UPDATE/PATCH an event'})
+})
+
+
+
+module.exports = router
